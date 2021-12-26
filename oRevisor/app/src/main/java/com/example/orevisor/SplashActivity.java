40d78be4e9ce38d1a9e5f447dbfe8ddcd3c7525b
mirndlazaro <mirandaalazaro@gmail.com>
2021-12-26 17:15:04 -0300

    Adicionar Google Analyitcs com integração ao Firebase ao projeto

--- oRevisor/app/src/main/java/com/example/orevisor/SplashActivity.java
+++ oRevisor/app/src/main/java/com/example/orevisor/SplashActivity.java
@@ -6,13 +6,30 @@ import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class SplashActivity extends AppCompatActivity {
     int tempoDeEspera = 1000 * 5;
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        FirebaseApp.initializeApp(SplashActivity.this);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(SplashActivity.this);
+
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.METHOD, "onCreate");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.LOGIN, bundle);
+
+        Bundle bundle1 = new Bundle();
+        bundle1.putString(FirebaseAnalytics.Param.ITEM_ID, "teste_id");
+        bundle1.putString(FirebaseAnalytics.Param.ITEM_NAME, "teste_name");
+        bundle1.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "teste_image");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle1);
 
         trocarTela();
     }
